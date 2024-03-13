9ee13ca3c3c4ef14b8025b836eb9b689e0479f68
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-03-13 15:22:49 +0700

    Bind FilmsManagement data to ViewModel instead of code-behind

--- WeMovieManager/View/FilmManagementView.xaml.cs
+++ WeMovieManager/View/FilmManagementView.xaml.cs
@@ -21,45 +21,13 @@ namespace WeMovieManager.View
     /// </summary>
     public partial class FilmManagementView : UserControl
     {
-        public ObservableCollection<Movie> MovieList { get; set; }
         public FilmManagementView()
         {
             InitializeComponent();
             // Initialize your MovieList
-            MovieList = new ObservableCollection<Movie>();
 
-            // Add some example movies
-            MovieList.Add(new Movie { DisplayName = "Movie 1", MovieType = "Type 1", Country = "Country 1", RunningTime = 120 });
-            MovieList.Add(new Movie { DisplayName = "Movie 2", MovieType = "Type 2", Country = "Country 2", RunningTime = 90 });
-            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
-            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
-            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
-            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
-            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
-            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
-            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
-            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
-
-            // Set the DataContext to this instance (for binding)
-            DataContext = this;
-        }
-
-        private void PencilIcon_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-        }
-
-
-        private void TrashIcon_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            // Handle trash icon click event here
         }
     }
 
-    public class Movie
-    {
-        public string DisplayName { get; set; }
-        public string MovieType { get; set; }
-        public string Country { get; set; }
-        public int RunningTime { get; set; }
-    }
+
 }
--- WeMovieManager/ViewModels/FilmsManagementViewModel.cs
+++ WeMovieManager/ViewModels/FilmsManagementViewModel.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace WeMovieManager.ViewModels
 {
     public class FilmsManagementViewModel : ViewModelBase
     {
+        public ObservableCollection<Movie> MovieList { get; set; }
+        private Movie _selectedItem;
+        public Movie SelectedItem
+        {
+            get
+            {
+                return _selectedItem;
+            }
+            set
+            {
+                _selectedItem = value;
+                Trace.WriteLine(value.DisplayName);
+                OnPropertyChanged(nameof(SelectedItem));
+            }
+        }
+
+        public FilmsManagementViewModel()
+        {
+            // Initialize your MovieList
+            MovieList = new ObservableCollection<Movie>();
+
+            // Add some example movies
+            MovieList.Add(new Movie { DisplayName = "Movie 1", MovieType = "Type 1", Country = "Country 1", RunningTime = 120 });
+            MovieList.Add(new Movie { DisplayName = "Movie 2", MovieType = "Type 2", Country = "Country 2", RunningTime = 90 });
+            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
+            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
+            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
+            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
+            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
+            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
+            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
+            MovieList.Add(new Movie { DisplayName = "Movie 3", MovieType = "Type 3", Country = "Country 3", RunningTime = 150 });
+
+            // Set the DataContext to this instance (for binding)
+        }
+
+        public class Movie
+        {
+            public string DisplayName { get; set; }
+            public string MovieType { get; set; }
+            public string Country { get; set; }
+            public int RunningTime { get; set; }
+        }
     }
 }
