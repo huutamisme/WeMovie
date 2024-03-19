985cac6c9c43be5f3001618ff8b66b7c472467c0
huutamisme <hohuutam287@gmail.com>
2024-03-19 22:28:55 +0700

    delete signup manager site, add press enter sign in, scroll to top, searchbox placeholder

--- WeMovieManager/Login.xaml.cs
+++ WeMovieManager/Login.xaml.cs
@@ -28,79 +28,8 @@ namespace WeMovieManager
         public Login(Window mainWindow)
         {
             InitializeComponent();
-            MyDatePicker.PreviewTextInput += DatePicker_PreviewTextInput;
             this.mainWindow = mainWindow;
 
-        }
-
-
-        private void DatePicker_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            DatePicker datePicker = sender as DatePicker;
-            if (datePicker == null)
-                return;
-
-            if (!char.IsDigit(e.Text, 0) && e.Text != "/")
-            {
-                e.Handled = true;
-                return;
-            }
-        }
-
-        private bool IsValidDate(string input)
-        {
-            DateTime date;
-            return DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
-        }
-
-        private async void SignUpButtonClick(object sender, RoutedEventArgs e)
-        {
-            string newText = MyDatePicker.Text;
-            string emailPattern = @"^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$";
-            if (emailBoxSignUp.Text.Length == 0)
-            {
-                txtBlockError.Text = "Please fill in all information fields";
-                ErrorFlyout.IsOpen = true;
-                ErrorFlyout.CloseButtonVisibility = Visibility.Hidden;
-                await Task.Delay(2000);
-                emailBoxSignUp.Focus();
-                return;
-            }
-            else if (!Regex.IsMatch(emailBoxSignUp.Text, emailPattern))
-            {
-                txtBlockError.Text = "Please input valid email!";
-                ErrorFlyout.IsOpen = true;
-                ErrorFlyout.CloseButtonVisibility = Visibility.Hidden;
-                await Task.Delay(2000);
-                emailBoxSignUp.Undo();
-                emailBoxSignUp.Focus();
-                return;
-            }
-            else if (passwordBoxSignUp.Password.Length == 0)
-            {
-                txtBlockError.Text = "Please fill in all information fields";
-                ErrorFlyout.IsOpen = true;
-                ErrorFlyout.CloseButtonVisibility = Visibility.Hidden;
-                await Task.Delay(2000);
-                passwordBoxSignUp.Focus();
-                return;
-            }
-            else if (!IsValidDate(newText))
-            {
-                txtBlockError.Text = "Invalid date format. Please enter a valid date in the format dd/mm/yyyy.";
-                ErrorFlyout.IsOpen = true;
-                ErrorFlyout.CloseButtonVisibility = Visibility.Hidden;
-                await Task.Delay(2000);
-                MyDatePicker.Focus();
-                return;
-            }
-            else
-            {
-                //sign up successfully
-                txtBlockFLyout.Text = "Sign up successfully!";
-                SuccessFlyout.IsOpen = true;
-                SuccessFlyout.CloseButtonVisibility = Visibility.Hidden;
-            }
         }
 
         private async void SignInButtonClick(object sender, RoutedEventArgs e)
@@ -145,7 +74,23 @@ namespace WeMovieManager
                         mainWindow.Show();
                     }
                 }
+
+                //txtBlockFLyout.Text = "Sign in successfully!";
+                //SuccessFlyout.IsOpen = true;
+                //SuccessFlyout.CloseButtonVisibility = Visibility.Hidden;
+                //await Task.Delay(1000);
+                //this.Close();
+                //mainWindow.Show();
             }
         }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SignInButtonClick(sender, e);
+            }
+        }
+
     }
 }
