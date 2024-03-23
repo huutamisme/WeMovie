0aa5570d6e24f2e0e5fb49cb83949ee5bf36c96b
huutamisme <hohuutam287@gmail.com>
2024-03-23 22:41:08 +0700

    finish report view

--- LoginForm/Report.xaml.cs
+++ LoginForm/Report.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LiveCharts.Definitions.Charts;
 
 namespace LoginForm
 {
@@ -24,168 +25,48 @@ namespace LoginForm
     /// </summary>
     public partial class Report : Window
     {
+
+        private Dictionary<string, int> movieDictionary = new Dictionary<string, int>
+        {
+            { "Phim 1", 10 },
+            { "Phim 2", 20 },
+            { "Phim 3", 15 },
+            { "Phim 4", 25 },
+            { "Phim 5", 18 },
+            { "Phim 6", 12 },
+            { "Phim 7", 30 },
+            { "Phim 8", 22 },
+            { "Phim 9", 17 },
+            { "Phim 10", 28 },
+            { "Phim 11", 13 },
+            { "Phim 12", 23 },
+            { "Phim 13", 19 },
+            { "Phim 14", 27 },
+            { "Phim 15", 16 },
+            { "Phim 16", 21 },
+            { "Phim 17", 24 },
+            { "Phim 18", 14 },
+            { "Phim 19", 29 },
+            { "Phim 20", 26 },
+            { "Phim 21", 11 },
+            { "Phim 22", 31 },
+            { "Phim 23", 36 },
+            { "Phim 24", 40 },
+            { "Phim 25", 35 },
+            { "Phim 26", 39 },
+            { "Phim 27", 33 },
+            { "Phim 28", 37 },
+            { "Phim 29", 34 },
+            { "Phim 30", 38 }
+        };
+
         public Report()
         {
             InitializeComponent();
-
-            DataContext = this;
-
-            MovieData = new SeriesCollection
-            {
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 2",
-                    Values = new ChartValues<int> { 20 } // Số lượng xuất chiếu của Phim 2
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 2",
-                    Values = new ChartValues<int> { 20 } // Số lượng xuất chiếu của Phim 2
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-                new PieSeries
-                {
-                    Title = "Phim 1",
-                    Values = new ChartValues<int> { 10 } // Số lượng xuất chiếu của Phim 1
-                },
-            };
+            LoadListBox();
+            UpdatePieChart();
         }
 
-        public SeriesCollection MovieData { get; set; }
 
         private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -216,6 +97,34 @@ namespace LoginForm
             else this.WindowState = WindowState.Normal;
         }
 
+        private void LoadListBox()
+        {
+            foreach (string movieName in movieDictionary.Keys)
+            {
+                phimListBox.Items.Add(movieName);
+            }
+        }
 
+        private void UpdatePieChart()
+        {
+            pieChart.Series = new SeriesCollection();
+
+            foreach (string selectedMovie in phimListBox.SelectedItems)
+            {
+                if (movieDictionary.ContainsKey(selectedMovie))
+                {
+                    pieChart.Series.Add(new PieSeries
+                    {
+                        Title = selectedMovie,
+                        Values = new ChartValues<int> { movieDictionary[selectedMovie] }
+                    });
+                }
+            }
+        }
+
+        private void phimListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdatePieChart();
+        }
     }
 }
