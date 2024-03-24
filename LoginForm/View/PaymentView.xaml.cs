41a6ce4ce771635620e6d2def08950cb48fb677a
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-03-25 02:27:27 +0700

    Hotfix for payment

--- LoginForm/View/PaymentView.xaml.cs
+++ LoginForm/View/PaymentView.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -31,7 +32,7 @@ namespace LoginForm.View
             InitializeComponent();
             PopulateListBoxWithSampleData();
         }
-
+        public static float totalDiscount;
         public static ObservableCollection<Voucher1> sampleData = new ObservableCollection<Voucher1>();
 
         private void PopulateListBoxWithSampleData()
@@ -39,14 +40,18 @@ namespace LoginForm.View
             payment = App.payment;
             string packUri = "pack://application:,,,/LoginForm;component" + payment.poster;
             imgToBind.Source = new ImageSourceConverter().ConvertFromString(packUri) as ImageSource;
-            sampleData.Add(new Voucher1 { Code = "KHLS1GG30H", VoucherInfoStr = "(Discount 30000)" });
-            sampleData.Add(new Voucher1 { Code = "KHLS1GG40H", VoucherInfoStr = "(Discount 40000)" });
             listBox.ItemsSource = sampleData;
             filmNameToBind.Text = payment.filmName;
             foreach (var item in payment.seats)
             {
                 Trace.WriteLine("In payment: " + item);
             }
+            seatToBind.Text = payment.seats.Count.ToString();
+            dateToBind.Content = payment.showDate;
+            timeToBind.Content = payment.showTime;
+            totalBeforeToBind.Content = payment.total;
+            totalAfterToBind.Content = payment.total;
+            priceToBind.Content = payment.price;
         }
 
         private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
@@ -58,17 +63,43 @@ namespace LoginForm.View
 
         private void AddVoucher_Click(object sender, RoutedEventArgs e)
         {
-            string voucherId = VoucherTextBox.Text;
+            string voucherCode = VoucherTextBox.Text;
 
-            if (string.IsNullOrEmpty(voucherId))
+            if (string.IsNullOrEmpty(voucherCode))
             {
-                new MessageBoxCustom("Error", "Please input your Voucher kjdchjhaksdjhsajksadh", MessageType.Error, MessageButtons.OK).ShowDialog();
+                new MessageBoxCustom("Error", "Please input your Voucher", MessageType.Error, MessageButtons.OK).ShowDialog();
             }
             else
             {
+                var query = from voucher in App.WeMovieDb.Vouchers where voucher.code == voucherCode select voucher;
                 // check invalid voucher
-                sampleData.Add(new Voucher1 { Code = voucherId, VoucherInfoStr = "(Discount 10000)" });
-                VoucherTextBox.Text = "";
+                var result = query.ToList();
+                if (result.Count == 0)
+                {
+                    new MessageBoxCustom("Error", "No matching voucher", MessageType.Error, MessageButtons.OK).ShowDialog();
+                }
+                else
+                {
+                    bool isExists = false;
+                    foreach (Voucher1 item in sampleData)
+                    {
+                        if (item.Code.Trim().ToLower().Equals(voucherCode.Trim().ToLower()))
+                        {
+                            isExists = true;
+                            break;
+                        }
+                    }
+                    if (isExists)
+                    {
+                        new MessageBoxCustom("Error", "Already applied this voucher", MessageType.Error, MessageButtons.OK).ShowDialog();
+                    }
+                    else
+                    {
+                        totalDiscount += (float)result[0].denomination;
+                        sampleData.Add(new Voucher1 { Code = result[0].code, VoucherInfoStr = "Discount " + result[0].denomination + "%", discount = (float)result[0].denomination, view = this });
+                        totalAfterToBind.Content = payment.total * (100 - totalDiscount) / 100;
+                    }
+                }
             }
 
         }
@@ -78,17 +109,35 @@ namespace LoginForm.View
             NavigateCommand BookingNavigateCommand = new NavigateCommand(new Services.NavigationService(App._navigationStore, () => { return new TicketBookingViewModel(); }));
             BookingNavigateCommand.Execute(this);
         }
+
+        public void substractDiscount()
+        {
+            totalAfterToBind.Content = payment.total * (100 - totalDiscount) / 100;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            foreach (int item in payment.seats)
+            {
+                var query = from seat in App.WeMovieDb.Seats where seat.id == item select seat;
+                var result = query.Single();
+                result.status = "Taken";
+                App.WeMovieDb.SaveChanges();
+            }
+        }
     }
 
     public class Voucher1
     {
         public RelayCommand delVoucherCommand => new RelayCommand(execute =>
         {
-            Trace.WriteLine(Code);
             PaymentView.sampleData.Remove(this);
-            
+            PaymentView.totalDiscount -= discount;
+            view.substractDiscount();
         }, canExecute => { return true; });
         public string Code { get; set; }
         public string VoucherInfoStr { get; set; }
+        public float discount;
+        public PaymentView view;
     }
 }
