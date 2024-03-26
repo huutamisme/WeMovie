0714b38a156d8577aa5547e4af1b141f09e34016
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-03-26 21:07:00 +0700

    Finished editing cast

--- WeMovieManager/EditCast.xaml.cs
+++ WeMovieManager/EditCast.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WeMovieManager.Commands;
+using WeMovieManager.Model;
+using WeMovieManager.Services;
+using WeMovieManager.ViewModels;
 
 namespace WeMovieManager
 {
@@ -19,9 +24,13 @@ namespace WeMovieManager
     /// </summary>
     public partial class EditCast : Window
     {
-        public EditCast()
+        public FilmActor Actor { get; set; }
+        public EditCast(FilmActor actor)
         {
+            Actor = actor;
             InitializeComponent();
+            bioToBind.Text = Actor.Bio;
+            castNameToBind.Text = Actor.ActorName;
         }
 
         private void CloseWindow_Click(object sender, RoutedEventArgs e)
@@ -31,7 +40,15 @@ namespace WeMovieManager
 
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
+            var query = from cast in App.WeMovieDb.Actors where cast.id == Actor.Id select cast;
+            var result = query.Single();
+            result.name = castNameToBind.Text;
+            result.biography = bioToBind.Text;
+            App.WeMovieDb.SaveChanges();
 
+            ICommand CastNavigateCommand = new NavigateCommand(new NavigationService(App._navigationStore, () => { return new CastManagementViewModel(); }));
+            CastNavigateCommand.Execute(this);
+            this.Close();
         }
     }
 }
